47cdad3fc9f8772800c42c61c4dcd2e85a5cf1ff
Guilherme Campos <gcamposadm@gmail.com>
2022-05-10 20:58:25 -0300

    first commit

---
+++ primeiro-programa-intellij/src/me/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import me.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello, World!");
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
